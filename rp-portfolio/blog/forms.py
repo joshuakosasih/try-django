487af27e3ac2bd5ef6e6aa37e1712b6f00c81f05
Joshua Kosasih <joshua.kosasih@gmail.com>
2019-07-03 21:27:31 +0700

    finished modifying comment function to use ajax

--- rp-portfolio/blog/forms.py
+++ rp-portfolio/blog/forms.py
@@ -4,12 +4,14 @@ class CommentForm(forms.Form):
     author = forms.CharField(
         max_length=60,
         widget=forms.TextInput(attrs={
+            "id": "f-author",
             "class": "form-control",
             "placeholder": "Your Name"
         })
     )
-    body = forms.CharField(widget=forms.Textarea(
-        attrs={
+    body = forms.CharField(
+        widget=forms.Textarea(attrs={
+            "id": "f-body",
             "class": "form-control",
             "placeholder": "Leave a comment!"
         })
